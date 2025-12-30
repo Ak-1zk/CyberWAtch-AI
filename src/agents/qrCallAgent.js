84b779d75603aef809dbb970e0facaa03efc82bb
Ak-1zk <azra07018@gmail.com>
2025-12-31 00:31:15 +0530

    Update qrCallAgent.js

--- src/agents/qrCallAgent.js
+++ src/agents/qrCallAgent.js
@@ -1,1 +1,11 @@
+exports.analyzeQRorCall = async (data) => {
+  let score = 0;
+  let reasons = [];
 
+  if (data.source === "unknown") {
+    score += 60;
+    reasons.push("Unknown source");
+  }
+
+  return { score, reasons };
+};
